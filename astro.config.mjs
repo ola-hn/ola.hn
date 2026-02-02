f74c33ff50cecefe9dd16d548bd5ba88a90377c4
Ola Hneini <ola.hneini@gmail.com>
2026-02-02 16:55:37 +0100

    ci: add workflow to deploy Astro website

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://ola-hn.github.io',
     server:{
         host: true
-    }
+    },
 });
