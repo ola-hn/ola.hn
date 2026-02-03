98b7d8a8512c13b09c9ec69acadf909138a58d79
Ola Hneini <ola.hneini@gmail.com>
2026-02-03 17:14:21 +0100

    ci: use custom domain to deploy site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ola-hn.github.io',
+    site: 'https://ola.hn',
     server:{
         host: true
     },
